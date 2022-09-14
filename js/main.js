0122f7b65721227b1ca6dcf63f25f9892a9c3967
atir75 <maria.rita27012@gmail.com>
2022-09-14 16:00:46 -0300

    commit bug qualification

--- js/main.js
+++ js/main.js
@@ -60,15 +60,19 @@ skillsHeader.forEach((el) =>{
 const tabs = document.querySelectorAll('[data-target]'),
         tabContents = document.querySelectorAll('[data-content]')
 
+        
 tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
         const target = document.querySelector(tab.dataset.target)
-
+        
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('qualification_active')
         })
         target.classList.add('qualification_active')
 
-        
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
     })
 })
